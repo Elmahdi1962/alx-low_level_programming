65dfcc765d475c30f33a602459b5ce385eedfc1d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-18 13:19:52 +0100

    fixed a silly mistake

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main - looks for the last digit of n and displays if its  * bigger than 5 or == 0 or less than 6 and not 0
+ * main - looks for the last digit of n and displays
+ * if its bigger than 5 or == 0 or less than 6 and
+ * not 0
  *
  * Return: 0
  */
@@ -16,13 +18,13 @@ int main(void)
 
 	last_digit = n % 10;
 	printf("Last digit of %d is %d ", n, last_digit);
-	if(last_digit > 5)
+	if (last_digit > 5)
 	{
 		printf("and is greater than 5\n");
-	} else if(last_digit == 0)
+	} else if (last_digit == 0)
 	{
 		printf("and is 0\n");
-	} else if(last_digit < 6 && last_digit != 0)
+	} else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
