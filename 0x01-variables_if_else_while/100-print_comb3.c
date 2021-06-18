b3a9d17539426a8c30af210a93f032140a769ef3
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-18 08:41:14 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits.
+ * Numbers must be separated by ,, followed by a space
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		for (n = 48 ; n < 58 ; n++)
+		{
+			if (n > i)
+			{
+				putchar(i);
+				putchar(n);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	return (0);
+}
