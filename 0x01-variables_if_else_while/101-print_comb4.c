dc73c489a31068944ef71330c3d4b49eee3257d0
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-18 09:31:34 -0700

    fixing

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,16 +23,16 @@ int main(void)
 				{
 					if (j > n)
 					{
-						putchar(i);
-						putchar(n);
-						putchar(j);
-						if (i == 55 && n == 56 && j == 57)
-						{
-							break;
-						}
+					putchar(i);
+					putchar(n);
+					putchar(j);
+					if (i == 55 && n == 56 && j == 57)
+					{
+						break;
+					}
 
-						putchar(',');
-						putchar(' ');
+					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
