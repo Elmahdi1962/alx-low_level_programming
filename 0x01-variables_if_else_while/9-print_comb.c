e07f1c9bf3b10e62df3f475e6f889ffb230dab36
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-18 07:45:53 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of
+ * single-digit numbers.separated by ,
+ * followed by a space
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n < 58 ; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
