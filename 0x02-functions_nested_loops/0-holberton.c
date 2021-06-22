8831c6b69447e06cd163f8c63df7978fc5879c6e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 03:14:41 -0700

    fixed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -10,15 +10,14 @@
 
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	char c[9] = "Holberton";
+	int i;
+
+	for(i = 0 ; i <= 9 ; i++)
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
+
 	return (0);
 }
