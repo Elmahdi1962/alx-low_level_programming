352f17ffd12e89c698ee3cfc3d1ff1cc5e2c3ccf
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 15:36:43 +0100

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,68 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 -  prints all natural numbers from n to 98,
+ * followed by a new line.
+ *
+ * @s: number to add
+ * @n: number to add
+ *
+ * Return: result
+ */
+
+void print_to_98(int n)
+{
+	int t;
+
+	t = 98;
+
+	if (n < t)
+	{
+		while (n < t)
+		{
+			if (n > 9 || n < -9)
+			{
+
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+				_putchar(',');
+				_putchar(' ');
+
+			} else
+			{
+
+				_putchar(n + 48);
+				_putchar(',');
+				_putchar(' ');
+
+			}
+			n++;
+		}
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}else
+	{
+			while (n > t)
+		{
+			if (n > 9 || n < -9)
+			{
+
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+				_putchar(',');
+				_putchar(' ');
+
+			} else
+			{
+
+				_putchar(n + 48);
+				_putchar(',');
+				_putchar(' ');
+
+			}
+			n--;
+		}
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+}
