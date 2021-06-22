a7a22926c7cea7e32a7cc3704752ebff0e947383
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 18:58:20 +0100

    task 15 code edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	long int i = 1, f1 = 1, f2 = 2, tmp;
+	long int i = 0, f1 = 1, f2 = 2, tmp;
 
 	while (i < 98)
 	{
 		tmp = f1 + f2;
 		f1 = f2;
 		f2 = tmp;
-		printf("%li, ", f2);
+		printf("%li, ", f1);
 		i++;
 	}
 	printf("\n");
