e1ac9c939a2ee76e84c8d02d7e6db320af4d7307
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 20:13:47 +0100

    task 16 code edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	unsigned long i = 0, f1 = 1, f2 = 2, tmp;
 
-	while (i < 99)
+	while (i < 101)
 	{
 		printf("%lu", f1);
 		tmp = f1 + f2;
 		f1 = f2;
 		f2 = tmp;
-		if(i < 98)
+		if(i < 100)
 		{
             printf(",  ");
 		}
