ec2e74da64993a2a006dcad96b132a31d09c48f3
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 17:31:17 +0100

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,38 +1,116 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
- * main - computes and prints the sum of all the multiples
- * of 3 or 5 below 1024 (excluded)
- *
- *
- * Return: 1
+ *powB - raises the number base to power power
+ *@base : the base
+ *@power : the power
+ *Return: return the answer
  */
+int powB(int base, int power)
+{
+	int i, prod = 1;
 
+	for (i = 0; i < power; i++)
+	{
+		prod = prod * base;
+	}
+	return (prod);
+}
+
+/**
+ *numLength - returns the lenth of string
+ *@num : operand number
+ *Return: number of digits
+ */
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+
+	return (length);
+}
+
+/**
+ *putnchar - Print a number of any any digit
+ *@num : takes an input number
+ *
+ *
+ */
+void putnchar(int num)
+{
+
+	int length = numLength(num), j = length - 1, k, tmp2, digit1;
+
+	if (num == 0)
+		_putchar(48);
+	else
+	{
+
+		while (j >= 0)
+		{
+			if (num % powB(10, j) == 0 && j != 0)
+			{
+				_putchar(48 + num / powB(10, j));
+				for (k = j; k > 0 ; k--)
+				{
+					_putchar(48);
+				}
+				j = -1;
+			}
+			else
+			{
+				digit1 = num / powB(10, j);
+				_putchar(digit1 + 48);
+				tmp2 = num;
+				num -= powB(10, j) * digit1;
+				if (numLength(tmp2) - numLength(num) == 2)
+				{
+					_putchar(48);
+					j--;
+				}
+				j--;
+			}
+		}
+	}
+
+}
+/**
+ * sumr - returns the sum of numbers from start to end with step steps
+ * @start: the first number
+ * @end: the last number
+ *@step:the number of  steps
+ * Return: sum
+ */
+int sumr(int start, int end, int step)
+{
+	int sum = 0, i;
+
+	for (i = start; i < end; i += step)
+	{
+		sum += i;
+	}
+	return (sum);
+
+}
+/**
+ * main - prints the sum of all numbers under 1024
+ * that are divisible by 3 or 5
+ * Return: 0
+ */
 int main(void)
 {
-	int result5, result3, i, tmp5, tmp3, sum;
 
-	for (i = 1; i < 1024; i++)
-	{
-		tmp5 = i * 5;
-		if (tmp5 < 1024)
-		{
-			result5 += tmp5;
-		}
-
-		tmp3 = i * 3;
-		if (tmp3 < 1024)
-		{
-			result3 += tmp3;
-		}
-
-		if (tmp5 > 1024 && tmp3 > 1024)
-		{
-			break;
-		}
-
-	}
-	sum = result5 + result3;
-	printf("%d---%d--- %d", result5, result3, sum);
-	return (1);
+	putnchar(sumr(3, 1024, 3) + sumr(5, 1024, 5) - sumr(15, 1024, 15));
+	_putchar('\n');
+	return (0);
 }
