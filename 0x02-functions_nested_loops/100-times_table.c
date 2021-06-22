a9885a1f779ecbbefdb4b6ccae256f9f39d5311f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 16:28:55 +0100

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: number to start from
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, r;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				r = (j * i);
+
+				if (r > 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((r / 10) + 48);
+					_putchar((r % 10) + 48);
+				} else if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(r + 48);
+				} else
+				{
+					_putchar(r + 48);
+				}
+				if (j != 9)
+				{
+				        _putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
