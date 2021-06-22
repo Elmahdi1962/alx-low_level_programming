32552b241e80c5fe0a1676f7bfd64de5dfe6005a
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 15:00:27 +0100

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
 
 /**
- * times_table - prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59.
+ * times_table -  prints the 9 times table, starting with 0.
  *
  *
  * Return: Void
@@ -10,20 +9,24 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, r;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
+			r = (j * i);
 			_putchar(' ');
 			_putchar(' ');
-			_putchar((j * i) + 48);
-			_putchar(',');
-			if (i == 9)
+			if (r > 9)
 			{
-				_putchar('$');
+				_putchar((r / 10) + 48);
+				_putchar((r % 10) + 48);
+			} else
+			{
+				_putchar(r + 48);
 			}
+			_putchar(',');
 		}
 		_putchar('\n');
 	}
