3ad45f85095eb193ae5aa74a1828f334500a5828
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 13:56:20 +0100

    task 8 fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,9 +21,9 @@ void jack_bauer(void)
 
 	for (h = 0; h <= 24; h++)
 	{
-		for (m = 0; m <= 59; m++)
+		for (m = 1; m <= 60; m++)
 		{
-			if (m == 0)
+			if (m == 60
 			{
 				_putchar(((h + 1) / 10) + 48);
 				_putchar(((h + 1) % 10) + 48);
