d4e0e4c7da2ab2baff7aa65a585a5ae2a058af67
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-22 13:46:44 +0100

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ *
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 24; h++ )
+	{
+		for (m = 0; m <= 60; m++)
+		{
+			if (m == 60)
+			{
+				_putchar(((h + 1) / 10) + 48);
+				_putchar(((h + 1) / 10) + 48);
+				_putchar(':');
+				_putchar('0');
+				_putchar('0');
+				_putchar('\n');
+			} else
+			{
+				_putchar((h / 10) + 48);
+				_putchar((h / 10) + 48);
+				_putchar(':');
+				_putchar((m / 10) + 48);
+				_putchar((m / 10) + 48);
+				_putchar('\n');
+			}
+		}
+	}
+}
