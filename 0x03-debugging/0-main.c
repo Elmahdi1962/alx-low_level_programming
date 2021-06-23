cf8a60829c1722066bcdd8c383e6e3a9df15d2f4
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-23 08:20:15 +0100

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio>
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+	printf("%d is zero", positive_or_negative(i););
+
+        return (0);
+}
