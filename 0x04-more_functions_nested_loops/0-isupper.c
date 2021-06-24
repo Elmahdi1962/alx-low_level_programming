81229af7adc2d1b9e928ca3368f60071c734e322
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-24 09:07:26 +0100

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include 'holberton.h'
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: charachter to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+
+int _isupper(int c)
+{
+	return (isupper(c));
+}
