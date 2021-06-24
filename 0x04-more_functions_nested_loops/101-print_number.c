347a2537980e954e289135fe4f2cbb922cebff4b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-24 06:16:51 -0700

    task 12 fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,30 @@
 
 void print_number(int n)
 {
+	int d = 1, i = 0, ii = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	while (n / d != 0)
+	{
+		d *= 10;
+		i++;
+	}
+	d = d / 10;
 
-	_putchar('0' + (n % 10));
+	while (ii < i)
+	{
+		_putchar('0' + n / d);
+		n = n - (n / d) * d;
+		d = d / 10;
+		ii++;
+	}
+
+	if (i == 0)
+		_putchar('0' + n);
+
 }
