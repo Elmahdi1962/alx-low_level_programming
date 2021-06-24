a5091ebec5391349ad141913aaaa54cf3765f14e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-24 05:41:54 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer using _putchar
+ *
+ * @n: integer to print
+ *
+ * Return: Void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar('0' + (n % 10));
+}
