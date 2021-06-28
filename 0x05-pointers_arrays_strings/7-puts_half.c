908a3132ee91bc8532001e1bff0150de6de70bcb
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-28 05:03:08 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of the string ,\n
+ *
+ * @str: string to print
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int length = _strlen(str), i;
+
+	for (i = (length - 1) / 2; i < length; i++)
+	{
+		_putchar(*(str + i));
+	}
+}
