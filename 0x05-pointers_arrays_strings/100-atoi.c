1f75a7c5e5315863e46f8ff5d8f7f71f3f847597
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-28 08:02:55 -0700

    task 10 fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,36 +2,26 @@
 
 /**
  * _atoi - convert a string to an integer
- *
  * @s: strign to convert
- *
  * Return: int
  */
-
-
 int _atoi(char *s)
 {
 	int i = 0, multiplier = 1, first_digit = -1, last_digit = -1, sign = 1;
 	unsigned int number = 0;
 
-	/*we loop thru the string*/
 	while (*(s + i) != 0)
 	{
-		/*we check first for a sigh if it exist*/
 		if (*(s + i) == '-')
 		{
 			sign *= -1;
 		}
-
-		/* check for numbers if they exist */
 		if (*(s + i) > 47 && *(s + i) < 58)
 		{
-			/* we check is it's the first digit */
 			if (!(*(s + i - 1) > 47 && *(s + i - 1) < 58))
 			{
 				first_digit = i;
 			}
-			/* we check if it's the last digit */
 			if (!(*(s + i + 1) > 47 && *(s + i + 1) < 58))
 			{
 				last_digit = i;
@@ -40,19 +30,14 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-	/*if not digits found exit with 0*/
 	if (first_digit == -1 && last_digit == -1)
-	{
 		return (0);
-	}
-	/* find how many digits the number has */
 	i = first_digit;
 	while (i < last_digit)
 	{
 		multiplier *= 10;
 		i++;
 	}
-	/*finding teh whole number*/
 	i = first_digit;
 	while (i <= last_digit)
 	{
@@ -61,6 +46,5 @@ int _atoi(char *s)
 		i++;
 	}
 	number *= sign;
-
 	return (number);
 }
