f78c1a3acc794171ed1f34142a1c0161042ae786
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-30 03:47:27 -0700

    task 5 and 4

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @
+ *
+ * Return: the result
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) &= ~' ';
+		i++;
+	}
+	return (s);
+}
