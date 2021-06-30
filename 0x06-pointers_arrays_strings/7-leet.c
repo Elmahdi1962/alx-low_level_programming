f5c559a23f144974cd5bf2ae301dae89751c332a
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-30 05:12:18 -0700

    done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * *leet - encodes a string into 1337
+ *
+ * @s: string to encode
+ *
+ * Return: result pointer
+ */
+
+char *leet(char *s)
+{
+	char letters[] = {'A', 'E', 'O', 'T', 'L'};
+	int numbers[] = {4, 3, 0, 7, 1};
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == letters[i] || *s == letters[i] + 32)
+				*s = numbers[i] + 48;
+		}
+		s++;
+	}
+	return (s);
+}
