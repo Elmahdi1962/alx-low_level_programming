b17a55bcced7d50e2a6e6a7bd290445cb47e7501
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-30 16:55:56 +0100

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,6 +2,52 @@
 
 
 /**
+ * int_to_str - function to convert int to string
+ *
+ * @num: integer to convert
+ * @str: string to write to
+ * @count: number of digits
+ * Return: pointer string
+ */
+char *int_to_str(unsigned long long int num, char *str, int count)
+{
+	str[count] = '\0';
+	while (count > 0)
+	{
+		if (count == 1)
+			str[count - 1] = '0' + num;
+		else
+			str[count - 1]  = '0' + (num % 10);
+		num /= 10;
+		count--;
+	}
+	return (str);
+
+
+
+/**
+ * str_to_int - convert str to int
+ *
+ * @s: string to convert
+ *
+ * Return: the int
+ */
+
+
+int str_to_int(char *s, l)
+{
+	int base = 1, tmp, i, number = 0;
+
+	for (i = l - 1; i <= 0; i--)
+	{
+		tmp = s[i] - '0';
+		number += (tmp * base);
+		base *= 10;
+	}
+	return (number);
+}
+
+/**
  * _strlen - return length of string
  *
  * @s: string to count
@@ -36,7 +82,22 @@ int _strlen(char *s)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int base = 1, num1 = 0, num2 = 0, l1, l2, i = 0;
+	int base = 1, i = 0, tmp, count = 0;
+	int l1 = _strlen(n1), l2 = _strlen(n2);
+	unsigned long long int result, num1 = 0, num2 = 0;
 
+	num1 = str_to_int(char n1, l1);
+	num2 = str_to_int(char n2, l2);
+	result = num1 + num2;
+
+	while (result > 0)
+	{
+		count++;
+		result /= 10;
+	}
+
+	if (count +1 > size_r)
+		return (0);
 	
+	return (*int_to_str(num1 + num2, *r, count));
 }
