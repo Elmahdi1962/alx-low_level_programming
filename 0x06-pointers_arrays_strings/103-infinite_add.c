331632543b79638b7d3f0bd9ccd0621f4a3fe6d1
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-06-30 08:00:38 -0700

    backup

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: string to count
+ *
+ * Return: the size
+ */
+
+
+int _strlen(char *s)
+{
+	int counter = 0;
+	char *n = s
+
+	while (*n != 0)
+	{
+		counter++;
+		n++;
+	}
+	return (counter);
+}
+
+
+/**
+ * *infinite_add - adds two numbers
+ * @n1: numbera
+ * @n2: number2
+ * @r: where to store result
+ * @size_r: r size
+ * Return: pointer to result
+ */
+
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int base = 1, num1 = 0, num2 = 0, l1, l2, i = 0;
+
+	
+}
