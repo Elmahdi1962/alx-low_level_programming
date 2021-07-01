01fb9632676fcc17d13ca6b8ec6a8846baf9af0b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-01 01:21:08 -0700

    fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen - return length of string
- *
- * @s: string to count
- *
- * Return: the size
- */
-
-
-int _strlen(char *s)
-{
-	int counter = 0;
-
-	while (*s != 0)
-	{
-		counter++;
-		s++;
-	}
-	return (counter);
-}
-
-
-
-
-/**
  * _strcmp - compares two strings.work exactly like strcmp
  *
  * @s1: string 1
@@ -37,7 +13,17 @@ int _strlen(char *s)
 
 int _strcmp(char *s1, char *s2)
 {
-	int sone = _strlen(s1), stwo = _strlen(s2);
+	int equal = 0;
 
-	return (sone == stwo ? 0 : sone > stwo ? 15 : -15);
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (equal);
 }
