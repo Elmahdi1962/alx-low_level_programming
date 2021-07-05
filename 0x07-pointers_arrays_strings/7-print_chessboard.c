06a0757ff66552fbe596d92831d0731c6a00865d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-05 02:42:29 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,13 +9,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
+	int i, j;
+
+	for (i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
-		if (i < 7)
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
