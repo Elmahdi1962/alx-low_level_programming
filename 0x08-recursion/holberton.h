17f1d28191c4efad39f5a98b8bd746e7cf338853
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-07 03:46:08 -0700

    task 7

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,3 +1,4 @@
+int _strlen(char *s);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -6,3 +7,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
