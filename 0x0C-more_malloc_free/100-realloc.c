c4cb18934a9e272eb8ac87fcd4b112641e361ad3
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-15 04:22:12 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be copied
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *r = dest;
+
+	while (n--)
+		*dest++ = *src++;
+	return (r);
+}
+
+
+
 /**
  * *_realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to previously allocated memory
@@ -13,5 +33,28 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *p;
 
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		p = malloc(new_size);
+		return (p);
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+
+	_memcpy(p, ptr, old_size);
+
+	free(ptr);
+
+	return p;
 }
