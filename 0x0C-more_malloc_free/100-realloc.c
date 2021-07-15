7169e5c50fa7e46d3aecda3c12f3edd8a2f1f6b2
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-15 03:43:43 -0700

    fixed

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to previously allocated memory
+ * @old_size: pointer's size
+ * @new_size: it's new size after realocating new memory
+ * Return: If new_size == old_size do not do anything and return ptr
+ * If new_size is equal to zero, and ptr is not NULL Return Null
+ */
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	unsigned int i;
+
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -22,5 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
+	for (i = 0; i < nmemb; i++)
+		*((int *)p + (i * sizeof(int))) = 0;
 	return (p);
 }
