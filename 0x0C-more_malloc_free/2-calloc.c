a55cddc1561632c126258016bda97e4c403dd0d8
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-15 02:48:46 -0700

    betty fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,5 +22,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	return p;
+	return (p);
 }
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *array_range - creates an array of integers from min to max
+ * @min: min value
+ * @max: max value
+ * Return: the pointer to the newly created array or null
+ * If min > max or If malloc fails
+ */
+
+
+int *array_range(int min, int max)
+{
+
+}
