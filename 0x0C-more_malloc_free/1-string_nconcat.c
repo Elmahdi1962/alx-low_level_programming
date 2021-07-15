e4292d0f647f7f5cecc841cb5a609584d394a7ab
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-15 12:20:30 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,29 @@
 
 
 /**
+ * _strlen - return length of string
+ *
+ * @s: string to count
+ *
+ * Return: the size
+ */
+
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
+
+
+
+/**
  * *string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
@@ -13,38 +36,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	unsigned int l1, l2, i, j;
+	char *p;
 
-	if (s1 == NULL)
-		s1 = "";
+	l1 = s1 != NULL ? _strlen(s1) : 0;
+	l2 = s2 != NULL ? _strlen(s2) : 0;
 
-	if (s2 == NULL)
-		s2 = "";
+	if (n >= l2)
+		n = l2;
 
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
+	p = malloc((l1 + n) * sizeof(char) + 1);
 
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-
-	if (n > ls2)
-		n = ls2;
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
+	for (i = 0; i < l1; i++)
+	{
+		p[i] = s1[i];
+	}
 
-	sout[i] = '\0';
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+		p[i] = s2[j];
 
-	return (sout);
+	p[i] = 0;
+	return (p);
 }
