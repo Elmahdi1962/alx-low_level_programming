bc8bd73b399c661025caad53b15db37ba9192ef0
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-15 11:59:38 -0700

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size + 1);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -22,9 +22,9 @@ int *array_range(int min, int max)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < n; i++, min++)
+	for (i = 0; i < n; i++)
 	{
-		p[i] = min;
+		p[i] = min++;
 	}
 
 	return (p);
