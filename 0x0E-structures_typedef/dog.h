3e9cfaa4c8435fba61e324ca58430513877e566b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-19 03:53:55 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - struct for dog
+ * @name: it's name
+ * @age: it's age
+ * @owner: it's owner
+ * description: it's a struct for a dog woof woof
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
