a9283241cd61f726b25b04f4764dee6bc49bf97b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-19 05:57:06 -0700

    task 4 fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,7 +7,7 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
- * return: Null if func failed or the new dog of type dog_t
+ * Return: Null if func failed or the new dog of type dog_t
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
