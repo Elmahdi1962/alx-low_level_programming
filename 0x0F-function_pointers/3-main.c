8661fffdad9ab74438209e7cbdef0b6999421614
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-20 10:57:18 -0700

    fixed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	op = argv[2];
-	if (get_op_func(op) == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
