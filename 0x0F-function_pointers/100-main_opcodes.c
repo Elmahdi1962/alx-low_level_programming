26996c758560cd3fb87261c7ed652c3ba5386daa
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-21 12:37:54 -0700

    fixed

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the opcodes of this function
- * @argc: The number of command-line arguments
- * @argv: The command-line arguments
- *
- * Return: 0 if successful
+ * main - prints the opcodes of its own main function.
+ * @argc: number of arguments
+ * @argv: array of arguments in string format
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	int n_bytes, i;
-	int (*prog)(int, char **) = &main;
+	int (*p)(int, char **) = &main;
+	int b, i;
 
-	if (argc == 2)
-	{
-		n_bytes = atoi(argv[1]);
-		if (n_bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-		for (i = 0; i < n_bytes; i++)
-			printf("%02x%c",
-				(unsigned char)*((char *)prog + i), i < n_bytes - 1 ? ' ' : '\n');
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	b = atoi(argv[1]);
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < b; i++)
+		printf("%02x%c",
+		       *((unsigned char *)(p + i)), i < b - 1 ? ' ' : '\n');
+	printf("\n");
 	return (0);
+
 }
