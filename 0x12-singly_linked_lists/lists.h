dd74e3252e8afc8db2fe0a8a1783348579881b77
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-29 09:33:25 +0000

    task 1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,5 +22,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
