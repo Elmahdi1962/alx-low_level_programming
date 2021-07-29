d67f924c504303ba98f324831b47248683841a86
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-29 11:14:16 +0000

    task 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,24 +1,5 @@
 #include "lists.h"
-
-/**
- * _strlen - return length of string
- *
- * @s: string to count
- *
- * Return: the size
- */
-
-
-int _strlen(char *s)
-{
-	int counter = 0;
-
-	while (s[counter] != 0)
-	{
-		counter++;
-	}
-	return (counter);
-}
+#include "_strlen.c"
 
 
 
