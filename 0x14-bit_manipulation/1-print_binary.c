50799e0ed36688f00e78e94dd1cf1815a757f07b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-05 10:27:30 +0000

    task 1 done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "holberton.h"
 
 /**
  * print_binary - prints the binary representation of a number.
@@ -11,5 +10,8 @@
 
 void print_binary(unsigned long int n)
 {
+	if (n > 1)
+		print_binary(n >> 1);
 
+	_putchar((n & 1) + '0');
 }
