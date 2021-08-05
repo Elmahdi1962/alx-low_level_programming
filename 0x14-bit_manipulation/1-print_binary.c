49b9f8eb8565c3102fffc476af7576c1200b80b8
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-08-05 09:17:06 +0000

    fix

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ *
+ * @n: a number
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+
+}
