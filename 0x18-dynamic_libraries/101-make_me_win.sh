bd69f81ad64c9c52638cd432af825cf54e22f1d1
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-20 13:40:12 +0100

    fixed

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$PWD/inject.so https://github.com/Elmahdi1962/alx-low_level_programming/blob/master/0x18-dynamic_libraries/inject.so
-export LD_PRELOAD=$PWD/inject.so
+wget -q --output-document=$PWD/inject.so https://github.com/Elmahdi1962/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib_inject.so
+export LD_PRELOAD=$PWD/lib_inject.so
