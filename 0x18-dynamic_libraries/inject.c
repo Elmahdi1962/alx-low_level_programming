44bfbbaa71b0c35f130a79dcff619cb97636e8de
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-20 13:27:31 +0100

    update

--- 0x18-dynamic_libraries/inject.c
+++ 0x18-dynamic_libraries/inject.c
@@ -1,17 +1,19 @@
-#include <stdlib.h>
 #include <unistd.h>
 
-/**
- * srand - Overwrites the srand function in the libc shared object
- * @seed: The seed to use for the random number generator. [Will be Ignored]
- */
-void srand(unsigned int seed)
-{
-	const char *nums_txt = "9 8 10 24 75 - 9\n";
-	const char *txt = "Congratulations, you win the Jackpot!\n";
 
-	(void)seed;
-	write(STDOUT_FILENO, (void *)nums_txt, 17);
-	write(STDOUT_FILENO, (void *)txt, 38);
-	exit(EXIT_SUCCESS);
+int printf(__attribute__((unused))const char *format, ...)
+{
+	static int f = 0;
+	if (!f)
+		write(1, "9 8 10 24 75 - 9\n", 17);
+
+	f = 1;
+	return (0);
+}
+
+
+int puts(__attribute__((unused))const char *s)
+{
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	return (0);
 }
