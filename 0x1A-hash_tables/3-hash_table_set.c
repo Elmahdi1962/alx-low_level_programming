791585aa9d8826bf6a6456755272e9af22a8f7b9
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-07 13:19:50 +0100

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: the key (can't be empty string)
+ * @value: value associated with the key (it can be empty string)
+ * Return: 1 on success 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int idx;
+	hash_node_t *hn;
+	char *k = malloc(sizeof(char) * strlen(key)), *val = malloc(sizeof(char) * strlen(value));
+	if (key == NULL || strlen(key) == 0)
+		return (0);
+
+	idx = key_index((const unsigned char *) key, ht->size);
+	hn = malloc(sizeof(hash_node_t));
+
+	if (hn == NULL)
+		return (0);
+
+	strcpy(k, key);
+	strcpy(val, value);
+	hn->key = k;
+	hn->value = val;
+
+	if (ht->array[idx] == NULL || ht->array[idx] == 0)
+	{
+		ht->array[idx] = hn;
+		hn->next = NULL;
+	} else
+	{
+		hn->next = ht->array[idx];
+		ht->array[idx] = hn;
+	}
+	return (1);
+}
