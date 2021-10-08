be7d3df812a01f538bf4b23924ae1e7059c2d773
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-10-08 14:52:53 +0100

    remove const casting

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,7 +9,7 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int idx = key_index((const unsigned char *) key, ht->size);
+	unsigned long int idx = key_index((unsigned char *) key, ht->size);
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || strlen(key) == 0)
