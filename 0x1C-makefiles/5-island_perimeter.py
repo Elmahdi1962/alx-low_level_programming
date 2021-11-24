d6522dca326b0651b89f5ffa83622c40b17cbda8
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-24 14:57:21 +0100

    fixed pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''task 5 module'''
 
+
 def island_perimeter(grid):
     '''returns the perimeter of the island described in grid'''
     total = 0
-    last_fc = [] # last list found cells indexes
+    last_fc = []  # last list found cells indexes
     current_fc = []
     for index, list in enumerate(grid):
         for idx, cell in enumerate(list):
@@ -12,17 +13,16 @@ def island_perimeter(grid):
                 current_fc.append(idx)
                 # right and left sides
                 if idx == 0 or list[idx - 1] == 0:
-                    total += 1 # add left side of the cell
+                    total += 1  # add left side of the cell
                 if idx == len(list) - 1 or list[idx + 1] == 0:
-                    total += 1 # add  right side of the cell
+                    total += 1  # add  right side of the cell
                 # top sides
                 if idx not in last_fc:
-                    total += 1 # add the top side
+                    total += 1  # add the top side
                 # bottom sides
                 if grid[index + 1][idx] != 1:
-                    total += 1 # add bottom side of the cell
+                    total += 1  # add bottom side of the cell
                 last_fc = current_fc.copy()
                 current_fc = []
-
 
     return total
