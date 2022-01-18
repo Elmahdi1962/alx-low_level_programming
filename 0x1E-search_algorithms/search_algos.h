89dc2c761477c3b22c8ec7e2ccc6c5a30d60eb7e
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-01-18 13:54:09 +0100

    task 11

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search_ex(int *array, int value, int first, int last);
+int advanced_binary(int *array, size_t size, int value);
+
 #endif
